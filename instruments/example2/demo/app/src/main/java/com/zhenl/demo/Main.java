bb3020aaba74d565e6900ea0dd605b560f13cc3b
Sasuke <zhenlin1993@gmail.com>
2018-12-29 14:52:23 +0800

    add keep alive

--- instruments/example2/demo/app/src/main/java/com/zhenl/demo/Main.java
+++ instruments/example2/demo/app/src/main/java/com/zhenl/demo/Main.java
@@ -1,5 +1,8 @@
 package com.zhenl.demo;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.webkit.WebView;
 
@@ -8,12 +11,41 @@ import com.taobao.android.dexposed.XC_MethodHook;
 
 public class Main {
 
+    private static GCDetect detect;
+
+    private static Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    Log.e("epic.hook", "keep alive is running");
+                    sendEmptyMessageDelayed(0, 10000);
+                    break;
+                case 1:
+                    init();
+                    break;
+            }
+        }
+    };
+
     public static void init() {
+        detect = new GCDetect();
+        handler.removeMessages(0);
+        handler.sendEmptyMessageDelayed(0, 10000);
         DexposedBridge.findAndHookMethod(WebView.class, "loadUrl", String.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                Log.e("epic hook", "loadUrl:" + param.args[0]);
+                Log.e("epic.hook", "loadUrl:" + param.args[0]);
             }
         });
+    }
+
+    static class GCDetect {
+        @Override
+        protected void finalize() throws Throwable {
+            Log.e("epic.hook", "Final redemption");
+            handler.sendEmptyMessageDelayed(1, 1000);
+            super.finalize();
+        }
     }
 }
